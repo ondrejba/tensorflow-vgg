bdc65301d2a79070cd0389fbbb747cef1612aca1
Ondrej Biza <ondrej.biza@gmail.com>
2017-11-21 17:40:56 +0100

    deep deconv

--- deconv_vgg16.py
+++ deconv_vgg16.py
@@ -35,7 +35,7 @@ filter_idx = tf.placeholder(tf.int32)
 vgg = vgg16.Vgg16()
 vgg.build(images)
 
-activations = tf.get_default_graph().get_operation_by_name("conv2_1/Conv2D").outputs[0]
+activations = tf.get_default_graph().get_operation_by_name("conv5_3/Conv2D").outputs[0]
 num_frames = activations.shape[-1].value
 deconv_img = vgg.debuild(activations, filter_idx)
 
